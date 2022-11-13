20a3ba0b4384bb58602a2e0f0261763764297480
Raj Shukla <rajdragatron918@gmail.com>
2022-11-13 00:42:47 -0500

    modified push

--- pages/contact.js
+++ pages/contact.js
@@ -1,25 +1,77 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import styles from '../styles/contact.module.css'
 const contact = () => {
+  const [name, setname] = useState('')
+  const [email, setemail] = useState('')
+  const [phone, setphone] = useState('')
+  const [desc, setdesc] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(phone, name, email, desc)
+    const data = { phone, name, email, desc };
+
+    fetch('http://localhost:3000/api/postcontact', {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.text())
+      .then(data => {
+        console.log('Success:', data);
+        alert("Thanks for contacting us");
+        setphone('')
+        setname('')
+        setdesc('')
+        setemail('')
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+
+  }
+  const handleChange = (e) => {
+    if (e.target.name == 'phone') {
+      setphone(e.target.value)
+    }
+    else if (e.target.name == 'email') {
+      setemail(e.target.value)
+    }
+    else if (e.target.name == 'desc') {
+      setdesc(e.target.value)
+    }
+    else if (e.target.name == 'name') {
+      setname(e.target.value)
+    }
+  }
   return (
-    <div>
-      <form>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
-        </div>
-        <div class="form-group form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
-    </div>
+    <div className={styles.container}>
+      <div className={styles.subcontainer}>
+        <h1>Contact Us</h1>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.mb3}>
+            <label htmlFor="name" className={styles.formlabel}>Enter your name</label>
+            <input type="text" value={name} onChange={handleChange} className="form-control" id="name" name='name' aria-describedby="emailHelp" />
+          </div>
+          <div className={styles.mb3}>
+            <label htmlFor="email" className={styles.formlabel}>Email address</label>
+            <input type="email" value={email} onChange={handleChange} className="form-control" name='email' id="email" aria-describedby="emailHelp" />
+            
+          </div>
+          <div className={styles.mb3}>
+            <label htmlFor="phone" className={styles.formlabel}>Password</label>
+            <input type="phone" value={phone} onChange={handleChange} className="form-control" name='phone' id="phone" />
+          </div>
+          <div className={styles.mb3}>
+            <label htmlFor="desc">Elaborate your concern</label>
+            <div className={styles.textareacontainer}><textarea value={desc} onChange={handleChange} className="form-control" placeholder="Write your concern here" name='desc' id="desc" /></div>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div >
+    </div >
   )
 }
 
